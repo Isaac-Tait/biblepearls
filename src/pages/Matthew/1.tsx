d93dc5c5f46df49b862b0e30d87192334e0788bf
Isaac Tait <isaac@mountaintopcoding.com>
2022-02-09 10:56:06 -0500

    feat: add Matthew 1

--- src/pages/Matthew/1.tsx
+++ src/pages/Matthew/1.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
@@ -10,64 +9,95 @@ export default function HomePage() {
       <Seo />
       <main>
         <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <footer className='absolute bottom-1 flex w-full flex-wrap justify-between px-1 text-gray-700'>
-              <div className='my-auto flex w-1/3 justify-start text-slate-500'>
-                <p className=''>
-                  © 2021 - {new Date().getFullYear()}, Built with&nbsp;
-                  <a
-                    href='https://nextjs.org/'
-                    className='text-indigo-500 hover:text-amber-400'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    Next.js
-                  </a>{' '}
-                  &&nbsp;
-                  <a
-                    href='https://tailwindcss.com'
-                    className='text-indigo-500 hover:text-lime-400'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    TailwindCSS
-                  </a>
-                  .
-                </p>
-              </div>
-              <div className='mt-2 flex w-1/3 justify-center'>
-                <a
-                  href='https://github.com/Isaac-Tait/ilovetofish'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <Image
-                    src='/images/GitHub.png'
-                    height={20}
-                    width={20}
-                    alt='GitHub'
-                  />
-                </a>
-              </div>
-              <div className='my-auto flex w-1/3 justify-end text-slate-500'>
-                <p className=''>
-                  Another&nbsp;
-                  <a
-                    href='https://mountaintopcoding.dev'
-                    className='text-indigo-500 hover:text-fuchsia-400'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    mountainTopCoding(
-                    <span role='img' aria-label='mountain with snow-cap'>
-                      &#127956;
-                    </span>
-                    );
-                  </a>{' '}
-                  project
-                </p>
-              </div>
-            </footer>
+          <h2 className='flex justify-center'>Matthew 1</h2>
+          <div className='mx-auto max-w-6xl rounded-xl bg-slate-200 px-2'>
+            <h3 className=''>The Genealogy of Jesus</h3>
+            <p>
+              <span className='pl-1 align-top text-xs text-red-500'>1</span>
+              &nbsp;The book of the genealogy of Jesus Christ, the son of David,
+              the son of Abraham.
+              <span className='pl-1 align-top text-xs text-red-500'>2</span>
+              &nbsp;Abraham became the father of Isaac. Isaac became the father
+              of Jacob. Jacob became the father of Judah and his brothers.
+              <span className='pl-1 align-top text-xs text-red-500'>3</span>
+              &nbsp;Judah became the father of Perez and Zerah by Tamar. Perez
+              became the father of Hezron. Hezron became the father of Ram.
+              <span className='pl-1 align-top text-xs text-red-500'>4</span>
+              &nbsp;Ram became the father of Amminadab. Amminadab became the
+              father of Nahshon. Nahshon became the father of Salmon.
+              <span className='pl-1 align-top text-xs text-red-500'>5</span>
+              &nbsp;Salmon became the father of Boaz by Rahab. Boaz became the
+              father of Obed by Ruth. Obed became the father of Jesse.
+              <span className='pl-1 align-top text-xs text-red-500'>6</span>
+              &nbsp;Jesse became the father of King David. David became the
+              father of Solomon by her who had been the wife of Uriah.
+              <span className='pl-1 align-top text-xs text-red-500'>7</span>
+              &nbsp;Solomon became the father of Rehoboam. Rehoboam became the
+              father of Abijah. Abijah became the father of Asa.
+              <span className='pl-1 align-top text-xs text-red-500'>8</span>
+              &nbsp;Asa became the father of Jehoshaphat. Jehoshaphat became the
+              father of Joram. Joram became the father of Uzziah.
+              <span className='pl-1 align-top text-xs text-red-500'>9</span>
+              &nbsp;Uzziah became the father of Jotham. Jotham became the father
+              of Ahaz. Ahaz became the father of Hezekiah.
+              <span className='pl-1 align-top text-xs text-red-500'>10</span>
+              &nbsp;Hezekiah became the father of Manasseh. Manasseh became the
+              father of Amon. Amon became the father of Josiah.
+              <span className='pl-1 align-top text-xs text-red-500'>11</span>
+              &nbsp;Josiah became the father of Jechoniah and his brothers, at
+              the time of the exile to Babylon.
+              <span className='pl-1 align-top text-xs text-red-500'>12</span>
+              &nbsp;After the exile to Babylon, Jechoniah became the father of
+              Shealtiel. Shealtiel became the father of Zerubbabel.
+              <span className='pl-1 align-top text-xs text-red-500'>13</span>
+              &nbsp;Zerubbabel became the father of Abiud. Abiud became the
+              father of Eliakim. Eliakim became the father of Azor.
+              <span className='pl-1 align-top text-xs text-red-500'>14</span>
+              &nbsp;Azor became the father of Sadoc. Sadoc became the father of
+              Achim. Achim became the father of Eliud.
+              <span className='pl-1 align-top text-xs text-red-500'>15</span>
+              &nbsp;Eliud became the father of Eleazar. Eleazar became the
+              father of Matthan. Matthan became the father of Jacob.
+              <span className='pl-1 align-top text-xs text-red-500'>16</span>
+              &nbsp;Jacob became the father of Joseph, the husband of Mary, from
+              whom was born Jesus, who is called Christ.
+              <span className='pl-1 align-top text-xs text-red-500'>17</span>
+              &nbsp;So all the generations from Abraham to David are fourteen
+              generations; from David to the exile to Babylon fourteen
+              generations; and from the carrying away to Babylon to the Christ,
+              fourteen generations.
+            </p>
+            <h3>The Birth of Jesus</h3>
+            <p>
+              <span className='pl-1 align-top text-xs text-red-500'>18</span>
+              &nbsp;Now the birth of Jesus Christ was like this; for after his
+              mother, Mary, was engaged to Joseph, before they came together,
+              she was found pregnant by the Holy Spirit.
+              <span className='pl-1 align-top text-xs text-red-500'>19</span>
+              &nbsp;Joseph, her husband, being a righteous man, and not willing
+              to make her a public example, intended to put her away secretly.
+              <span className='pl-1 align-top text-xs text-red-500'>20</span>
+              &nbsp;But when he thought about these things, behold, an angel of
+              the Lord appeared to him in a dream, saying, &quot;Joseph, son of
+              David, don&#39;t be afraid to take to yourself Mary, your wife,
+              for that which is conceived in her is of the Holy Spirit.
+              <span className='pl-1 align-top text-xs text-red-500'>21</span>
+              &nbsp;She shall bring forth a son. You shall call his name Jesus,
+              for it is he who shall save his people from their sins.&quot;
+              <span className='pl-1 align-top text-xs text-red-500'>22</span>
+              &nbsp;Now all this has happened, that it might be fulfilled which
+              was spoken by the Lord through the prophet, saying,
+              <span className='pl-1 align-top text-xs text-red-500'>23</span>
+              &nbsp;&quot;Behold, the virgin shall be with child, and shall
+              bring forth a son. They shall call his name Immanuel;&quot; which
+              is, being interpreted, &quot;God with us.&quot;
+              <span className='pl-1 align-top text-xs text-red-500'>24</span>
+              &nbsp;Joseph arose from his sleep, and did as the angel of the
+              Lord commanded him, and took his wife to himself;
+              <span className='pl-1 align-top text-xs text-red-500'>25</span>
+              &nbsp;and didn&#39;t know her sexually until she had brought forth
+              her firstborn son. He named him Jesus.
+            </p>
           </div>
         </section>
       </main>
